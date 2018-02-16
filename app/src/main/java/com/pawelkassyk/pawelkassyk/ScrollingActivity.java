6821acc6d90b70f31ba4375a205052c21cd001bb
Pawel Kassyk <pkassyk@gmail.com>
2018-02-16 17:34:41 +0100

    github website added to github button

--- app/src/main/java/com/pawelkassyk/pawelkassyk/ScrollingActivity.java
+++ app/src/main/java/com/pawelkassyk/pawelkassyk/ScrollingActivity.java
@@ -1,5 +1,7 @@
 package com.pawelkassyk.pawelkassyk;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -22,8 +24,8 @@ public class ScrollingActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://github.com/pawelkassyk"));
+                startActivity(browserIntent);
             }
         });
     }
